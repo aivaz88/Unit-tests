e3798fba319155602567de1090f037d93be41c23
aivaz88 <aivaz1988@mail.ru>
2024-01-28 20:42:21 +0530

    lesson 1

---
+++ lesson1/src/seminars/first/model/Calculator.java
@@ -0,0 +1,62 @@
+package seminars.first.model;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    public static double squareRootExtraction(double number) {
+        if (number == 0) {
+            throw new ArithmeticException("It is not possible to extract the root from 0");
+        }
+        if (number < 0) {
+            throw new ArithmeticException("It is impossible to extract the root from negative numbers");
+        }
+
+        double t;
+        double squareRoot = number / 2;
+        do {
+            t = squareRoot;
+            squareRoot = (t + (number / t)) / 2;
+        }
+        while ((t - squareRoot) != 0);
+        return squareRoot;
+
+        // или просто return Math.sqrt(number);
+    }
+
+    /**
+     * @param purchaseAmount сумма покупки
+     * @param discountAmount размер скидки
+     * @return возвращает сумму покупки с учетом скидки
+     */
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        assertThat(discountAmount<100).isInstanceOf(ArithmeticException.class);
+        assertThat(discountAmount>100).isInstanceOf(ArithmeticException.class);
+        return purchaseAmount*(1-discountAmount/100);
+    }
+}
